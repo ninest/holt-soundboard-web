abf3b2a8228ce298b43ff7f4995b0ea4867e4c21
themindstorm <parthkabra@Parths-MacBook-Air.local>
2019-09-11 11:35:53 +0800

    fixed links

--- pages/known_issues.vue
+++ pages/known_issues.vue
@@ -4,8 +4,8 @@
     main 
       section(v-html="knownIssuesHtml").text
       section.button-group
-        a.button(href="") New GitHub Issue
-        a.button(href="").red Post on Reddit
+        a.button(target="_blank" href="https://github.com/holt-soundboard/holt-soundboard-web/issues/new") New GitHub Issue
+        a.button(target="_blank" href="reddit.com/r/holt_soundboard").red Post on Reddit
 </template>
 
 <script>
